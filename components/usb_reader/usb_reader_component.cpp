ec849bac8923be20316cca5d5371a34c4b99b7eb
marcob79 <66200214+marcob79@users.noreply.github.com>
2025-09-10 14:01:30 +0200

    Delete components/usb_reader directory

--- components/usb_reader/usb_reader_component.cpp
+++
@@ -1,9 +0,0 @@
-#include "usb_reader.h"
-
-namespace esphome {
-namespace usb_reader {
-
-// Nessuna logica extra qui, tutto gestito in usb_reader.cpp
-
-}  // namespace usb_reader
-}  // namespace esphome
